34224c6466bf423b65ce625b22c92efc59a54c04
unknown <ankitkp028>
2022-10-27 20:59:30 +0530

    nav+footer

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import "./common.css";
 
 import NavBar from "./Components/NavBar";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
-    <div>
+    <>
       <NavBar />
-    </div>
+      <Footer />
+    </>
   );
 }
 
